9ffb9b53ba26df612c4f8638cbf64f1f62def22f
sung_jee <lsvk3577@naver.com>
2022-05-15 05:15:07 +0900

    Update fs.c

--- filesystem/fs.c
+++ filesystem/fs.c
@@ -4,98 +4,142 @@
 #include "disk.h"
 #include "fs.h"
 
-
-void FileSySInit(){
-    DevCreateDisk();
-	
-	char*a=(char*)malloc(sizeof(char)*BLOCK_SIZE);
-	
+void FileSysInit(void){
+	DevCreateDisk();
+	char * a = (char*)malloc(sizeof(char)*BLOCK_SIZE);
 
 	memset(a,0,BLOCK_SIZE);
-
+	
 
 	for(int i=0;i<512;i++){
-		char*a=(char*)malloc(sizeof(char)*BLOCK_SIZE);
 		DevWriteBlock(i,a);
 	}
+	
 }
 
 
-//이노드 바이트맵 즉 블럭 1번
-//블럭당 바이트 512니깐
-//64개?의 파일들에 대한 inode 저장 인지
-//1바이트에 저장되니깐
-//512개의 파일에 대한 설정정보를 저장하는지..
 void SetInodeBytemap(int inodeno)
 {
-    char* content;
-    DevReadBlock(INODE_BYTEMAP_BLOCK_NUM,content);
-
-    for(int i=0;i<64;i++){
-        printf("%c",content[i]);
-    }
+	char* content=(char*)malloc(sizeof(char)*BLOCK_SIZE);
+	DevReadBlock(INODE_BYTEMAP_BLOCK_NUM,content);
 
 
-    content[inodeno]=1;
-    
-    for(int i=0;i<64;i++){
-        printf("%c",content[i]);
-    }
-    
+
+	content[inodeno]=1;
+	DevWriteBlock(INODE_BYTEMAP_BLOCK_NUM,content);
 
 
-    DevWriteBlock(INODE_BYTEMAP_BLOCK_NUM,content);
 }
 
 
 void ResetInodeBytemap(int inodeno)
 {
-    char*content;
-    DevReadBlock(INODE_BYTEMAP_BLOCK_NUM,content);
+	char*content=(char*)malloc(sizeof(char)*BLOCK_SIZE);
+	DevReadBlock(INODE_BYTEMAP_BLOCK_NUM,content);
+	
+	
 
-    content[inodeno]=0;
 
-    DevWriteBlock(INODE_BYTEMAP_BLOCK_NUM,content);
 
+	content[inodeno]=0;
+	
+
+	DevWriteBlock(INODE_BYTEMAP_BLOCK_NUM,content);
 }
 
 
 void SetBlockBytemap(int blkno)
 {
-    char*content;
-    DevReadBlock(INODE_BYTEMAP_BLOCK_NUM,content);
+	char*content=(char*)malloc(sizeof(char)*BLOCK_SIZE);
+	DevReadBlock(INODE_BYTEMAP_BLOCK_NUM,content);
 
-    content[blkno]=1;
+	content[blkno]=1;
 
-    DevWriteBlock(INODE_BYTEMAP_BLOCK_NUM,content);
-
-
+	DevWriteBlock(INODE_BYTEMAP_BLOCK_NUM,content);
 }
 
 
 void ResetBlockBytemap(int blkno)
 {
-    char*content;
-    DevReadBlock(INODE_BYTEMAP_BLOCK_NUM,content);
-    content[blkno]=0;
-    DevWriteBlock(INODE_BYTEMAP_BLOCK_NUM,content);
+	char*content=(char*)malloc(sizeof(char)*BLOCK_SIZE);
+	DevReadBlock(INODE_BYTEMAP_BLOCK_NUM,content);
+	content[blkno]=0;
+	DevWriteBlock(INODE_BYTEMAP_BLOCK_NUM,content);
 }
 
 
 void PutInode(int inodeno, Inode* pInode)
-{
+{	
+	char*content=(char*)malloc(sizeof(char)*BLOCK_SIZE);
+
+	// find block
+	if(0<=inodeno&&inodeno<=15){
+        DevReadBlock(INODELIST_BLOCK_FIRST,content);
+		
+    }else if(16<=inodeno&&inodeno<=31){
+        DevReadBlock(INODELIST_BLOCK_FIRST+1,content);
+    }else if(32<=inodeno&&inodeno<=47){
+        DevReadBlock(INODELIST_BLOCK_FIRST+2,content);
+    }else if(48<=inodeno&&inodeno<=63){
+        DevReadBlock(INODELIST_BLOCK_FIRST+3,content);
+    }
+
+	// variable
+	int allocBlocks=pInode->allocBlocks;
+    int size=pInode->size;
+    int type=pInode->type;
+    int dirBlockPtr1=pInode->dirBlockPtr[0];
+    int dirBlockPtr2=pInode->dirBlockPtr[1];
+    int dirBlockPtr3=pInode->dirBlockPtr[2];
+    int dirBlockPtr4=pInode->dirBlockPtr[3];
+    int indirectBlockPtr=pInode->indirectBlockPtr;
+
+	int idx=(inodeno%16)*8;
+	
+
+	memcpy(content+(idx)*4,&allocBlocks,sizeof(int));
+    memcpy(content+(idx+1)*4,&size,sizeof(int));
+    memcpy(content+(idx+2)*4,&type,sizeof(int));
+    memcpy(content+(idx+3)*4,&dirBlockPtr1,sizeof(int));
+    memcpy(content+(idx+4)*4,&dirBlockPtr2,sizeof(int));
+    memcpy(content+(idx+5)*4,&dirBlockPtr3,sizeof(int));
+    memcpy(content+(idx+6)*4,&dirBlockPtr4,sizeof(int));
+    memcpy(content+(idx+7)*4,&indirectBlockPtr,sizeof(int));
+	
+
+	if(0<=inodeno&&inodeno<=15){
+		DevWriteBlock(INODELIST_BLOCK_FIRST,content);
+    }else if(16<=inodeno&&inodeno<=31){
+        DevWriteBlock(INODELIST_BLOCK_FIRST+1,content);
+    }else if(32<=inodeno&&inodeno<=47){
+        DevWriteBlock(INODELIST_BLOCK_FIRST+2,content);
+    }else if(48<=inodeno&&inodeno<=63){
+        DevWriteBlock(INODELIST_BLOCK_FIRST+3,content);
+    }
+
+	
+
+	
+
 
 }
 
 
 void GetInode(int inodeno, Inode* pInode)
-{   
-    // 512바이트 = content 
-    char*content;
-    // 블록당 512바이트니깐 
-    // 블록은 연속적이다..
-    DevReadBlock(INODELIST_BLOCK_FIRST,content);
-    int allocBlocks;
+{
+	char*content=(char*)malloc(sizeof(char)*BLOCK_SIZE);
+
+	if(0<=inodeno&&inodeno<=15){
+        DevReadBlock(INODELIST_BLOCK_FIRST,content);
+    }else if(16<=inodeno&&inodeno<=31){
+        DevReadBlock(INODELIST_BLOCK_FIRST+1,content);
+    }else if(32<=inodeno&&inodeno<=47){
+        DevReadBlock(INODELIST_BLOCK_FIRST+2,content);
+    }else if(48<=inodeno&&inodeno<=63){
+        DevReadBlock(INODELIST_BLOCK_FIRST+3,content);
+    }
+	// variable
+	int allocBlocks;
     int size;
     int type;
     int dirBlockPtr1;
@@ -104,10 +148,11 @@ void GetInode(int inodeno, Inode* pInode)
     int dirBlockPtr4;
     int indirectBlockPtr;
 
-    // 여기서 부터 할당
-    int idx=inodeno*8;
+	int idx=(inodeno%16)*8;
+	
+	
 
-    memcpy(&allocBlocks,content+(idx)*4,sizeof(int));
+	memcpy(&allocBlocks,content+(idx)*4,sizeof(int));
     memcpy(&size,content+(idx+1)*4,sizeof(int));
     memcpy(&type,content+(idx+2)*4,sizeof(int));
     memcpy(&dirBlockPtr1,content+(idx+3)*4,sizeof(int));
@@ -115,16 +160,22 @@ void GetInode(int inodeno, Inode* pInode)
     memcpy(&dirBlockPtr3,content+(idx+5)*4,sizeof(int));
     memcpy(&dirBlockPtr4,content+(idx+6)*4,sizeof(int));
     memcpy(&indirectBlockPtr,content+(idx+7)*4,sizeof(int));
+
+	
+	
+
     pInode->allocBlocks=allocBlocks;
     pInode->size=size;
     pInode->type=type;
     pInode->indirectBlockPtr=indirectBlockPtr;
-    // 이부분 수정 필요
-    pInode->dirBlockPtr[0]=dirBlockPtr1;
+	
+
+	pInode->dirBlockPtr[0]=dirBlockPtr1;
     pInode->dirBlockPtr[1]=dirBlockPtr2;
     pInode->dirBlockPtr[2]=dirBlockPtr3;
     pInode->dirBlockPtr[3]=dirBlockPtr4;
-        
+
+
 
 }
 
