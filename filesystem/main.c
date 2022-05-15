a6ef21749fdc27f07c84592547276d0b3effb96e
sung_jee <lsvk3577@naver.com>
2022-05-15 18:26:22 +0900

    Update main.c

--- filesystem/main.c
+++ filesystem/main.c
@@ -2,12 +2,30 @@
 #include "disk.h"
 #include <stdio.h>
 #include<stdlib.h>
-
 #include<string.h>
 int main(){
 	
 	FileSysInit();
-	
+
+	struct __dirEntry * direntry=malloc(32);
+	// char b[MAX_NAME_LEN]="asdgasdasg";
+	// direntry->name=b;
+	strcpy(direntry->name,"akakakakak");
+	direntry->inodeNum=14;
+
+	PutDirEntry(131,3,direntry);
+
+	struct __dirEntry * direntry2=malloc(32);
+	GetDirEntry(131,3,direntry2);
+
+	printf("%d",direntry2->inodeNum);
+
+
+
+	// PutIndirectBlockEntry(100,5,129);
+	// int tmp=GetIndirectBlockEntry(100,5);
+	// printf("%d",tmp);
+
 
 	// struct _Inode *inode1=malloc(32);
 	// inode1->allocBlocks=132;
@@ -19,28 +37,57 @@ int main(){
 	// inode1->dirBlockPtr[3]=13;
 	// inode1->indirectBlockPtr=17;
 	
-	// PutInode(0,inode1);
+	// PutInode(14,inode1);
+
+	/////
+	// char* a =malloc(512);
+
+	// DevReadBlock(3,a);
+
 
 	
+// a
+	//memcpy(&c,a+48,sizeof(int));
+
+	// memcpy(&c,a,sizeof(int));
+	
+
+
+
+
+
+
+	// printf("%d",inode2->size);
+
+
+	//struct _Inode *inode2=malloc(32);
+	
+
+	//GetInode(14,inode2);
+
+
+
+
+
+
+
+
+
+
+
+	// for(int i=0;i<13;i++){
+	// 	SetBlockBytemap(i);
+	// }
+	// int a=GetFreeBlockNum();
+	// printf("%d\n",a);
 	// // DevReadBlock(INODELIST_BLOCK_FIRST,a);
 	// // for(int i=0;i<32;i+=4){
 	// // 	int b;
 	// // 	memcpy(&b,a+i,sizeof(int));
 	// // 	printf("%d\n",b);
 	// // }
-	
-	
-	// struct _Inode *inode2=malloc(32);
-	// // memset(inode2,0,32);
 
-	// GetInode(0,inode2);
-	// printf("%d",inode2->size);
 
-	for(int i=0;i<13;i++){
-		SetBlockBytemap(i);
-	}
-	int a=GetFreeBlockNum();
-	printf("%d\n",a);
 
-	return 0;
+	// return 0;
 }
